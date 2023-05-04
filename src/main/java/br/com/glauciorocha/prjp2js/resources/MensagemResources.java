6e1a941271539fb6cd9343a4bb4f43bd13bba51c
Gláucio Rocha <glauciobr@gmail.com>
2023-05-04 10:28:57 -0300

    Ativando CORs

--- src/main/java/br/com/glauciorocha/prjp2js/resources/MensagemResources.java
+++ src/main/java/br/com/glauciorocha/prjp2js/resources/MensagemResources.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import br.com.glauciorocha.prjp2js.repository.MensagemRepository;
 
 @RequestMapping(value="/mensagens")
 @RestController
+@CrossOrigin
 public class MensagemResources {
 
 	@Autowired
--- src/main/java/br/com/glauciorocha/prjp2js/resources/UsuarioResources.java
+++ src/main/java/br/com/glauciorocha/prjp2js/resources/UsuarioResources.java
@@ -3,6 +3,7 @@ package br.com.glauciorocha.prjp2js.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +14,7 @@ import br.com.glauciorocha.prjp2js.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping(value="/usuarios")
+@CrossOrigin
 public class UsuarioResources {
 
 	@Autowired
