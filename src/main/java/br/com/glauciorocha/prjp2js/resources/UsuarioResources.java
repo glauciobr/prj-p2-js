13351a649727b8bc23b108e4fb464a9066296b28
Gláucio Rocha <glauciobr@gmail.com>
2023-05-11 17:38:29 -0300

    ajustes

--- src/main/java/br/com/glauciorocha/prjp2js/resources/UsuarioResources.java
+++ src/main/java/br/com/glauciorocha/prjp2js/resources/UsuarioResources.java
@@ -14,6 +14,7 @@ import br.com.glauciorocha.prjp2js.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping(value="/usuarios")
+@CrossOrigin
 public class UsuarioResources {
 
 	@Autowired
